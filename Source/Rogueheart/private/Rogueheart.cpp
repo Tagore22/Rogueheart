771938524a00adad3a81816b3612ccf8f57b32e2
홍지학 <5226947@hanmail.net>
2025-07-08 23:04:26 +0900

    Delete Source/Rogueheart/private directory

--- Source/Rogueheart/private/Rogueheart.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Rogueheart.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rogueheart, "Rogueheart" );
