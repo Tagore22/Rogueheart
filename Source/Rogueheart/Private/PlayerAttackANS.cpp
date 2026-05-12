f9ab0bf2b32ded225f32a6475a34d6eaaded3732
홍지학 <5226947@hanmail.net>
2026-05-12 23:01:15 +0900

    Update 2026. 5. 12 PlayerAttackANS Refactoring

---
+++ Source/Rogueheart/Private/PlayerAttackANS.cpp
@@ -0,0 +1,16 @@
+#include "PlayerAttackANS.h"
+
+void UPlayerAttackANS::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+
+}
+
+void UPlayerAttackANS::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+
+}
+
+void UPlayerAttackANS::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+
+}
---
+++ Source/Rogueheart/Public/PlayerAttackANS.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "PlayerAttackANS.generated.h"
+
+UCLASS()
+class ROGUEHEART_API UPlayerAttackANS : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+protected:
+	// 시작 시 1회 호출
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+
+	// 구간 내 매 프레임 호출
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+
+	// 끝날 때 1회 호출
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+};
