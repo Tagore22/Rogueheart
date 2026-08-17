60420d92f10ac0faccf075a95f3c9d1211a08f30
홍지학 <5226947@hanmail.net>
2026-08-17 23:47:13 +0900

    Update 2026. 8. 17 UAnimNotifyState Add.

---
+++ Source/Rogueheart/Private/EnemyBossSmashANS.cpp
@@ -0,0 +1,47 @@
+#include "EnemyBossSmashANS.h"
+#include "Character/Enemy/EnemyBoss.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UEnemyBossSmashANS::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	AEnemyBoss* OwnActor = Cast<AEnemyBoss>(MeshComp->GetOwner());
+	if (!IsValid(OwnActor))
+	{
+		return;
+	}
+
+	MoveCom = OwnActor->GetCharacterMovement();
+	if (!MoveCom)
+	{
+		return;
+	}
+
+	MoveCom->SetMovementMode(MOVE_Falling);
+}
+
+void UEnemyBossSmashANS::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+}
+
+void UEnemyBossSmashANS::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	AEnemyBoss* OwnActor = Cast<AEnemyBoss>(MeshComp->GetOwner());
+	if (!IsValid(OwnActor))
+	{
+		return;
+	}
+
+	MoveCom = OwnActor->GetCharacterMovement();
+	if (!MoveCom)
+	{
+		return;
+	}
+
+	MoveCom->SetMovementMode(MOVE_Walking);
+}
+
