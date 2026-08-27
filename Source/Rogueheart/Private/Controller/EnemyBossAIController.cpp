12cf4756fa8fc81832990a15db1c0c016acf44c3
홍지학 <5226947@hanmail.net>
2026-08-27 23:51:01 +0900

    Update 2026. 8. 27 EnemyBossAI Refactoring

---
+++ Source/Rogueheart/Private/Controller/EnemyBossAIController.cpp
@@ -0,0 +1,6 @@
+#include "Controller/EnemyBossAIController.h"
+
+AEnemyBossAIController::AEnemyBossAIController()
+{
+	// c++에서의 하드코딩보다 BP와 EditDefaultsOnly 매크로를 이용하여 에디터에서 BB 및 BT를 연동한다.
+}
---
+++ Source/Rogueheart/Public/Controller/EnemyBossAIController.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Controller/EnemyAIController.h"
+#include "EnemyBossAIController.generated.h"
+
+UCLASS()
+class ROGUEHEART_API AEnemyBossAIController : public AEnemyAIController
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyBossAIController();
+};
